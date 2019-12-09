3cb2c5bfdbdda1aaceec6ec6c374e4bf1a5e00d1
EC2 Default User <ec2-user@ip-172-31-94-227.ec2.internal>
2019-12-09 18:24:16 +0000

    adding map markers

---
+++ assets/js/map.js
@@ -0,0 +1,25 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 9,
+        center: { lat: 44.434546, lng: -79.350972 }
+    });
+}
+
+var label = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+var locations = [
+    { lat: 44.302020, lng: -79.472088 },
+    { lat: 44.308918, lng: -79.437413 }
+    ];
+    
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker ({
+            position: locations,
+            label: label[i % label.length]
+        });
+    });
+    
+    var markerClusterer = new MarkerClusterer (map, markers, {
+        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer/m'
+    });
+    
