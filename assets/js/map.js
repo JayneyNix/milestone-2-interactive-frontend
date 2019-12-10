05268c423f7c13c271052d9f3879dc9eededc5a6
EC2 Default User <ec2-user@ip-172-31-94-227.ec2.internal>
2019-12-10 15:21:13 +0000

    favicon file added to assets & link in html

--- assets/js/map.js
+++ assets/js/map.js
@@ -4,39 +4,23 @@ function initMap() {
         center: { lat: 44.434546, lng: -79.350972 }
   
  });
+var labels = "AB";
 
-setMarkers(map);
-}
-var marinas = [
-    ['Island Grove Marina', 44.302020, -79.472088, 2],
-    ['Willow Beach Marina', 44.308918, -79.437413, 1],
+var locations = [
+    { lat: 44.302020, lng: -79.472088 },
+    { lat: 44.308918, lng: -79.437413 }
     ];
     
-    function setMarkers(map) {
-        var image = {
-            url: 'assets/images/anchor.png',
-            size: new google.maps.Size(30, 40),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(40, 0)
-        };
-    
-    var shape = {
-        coords: [1, 1, 1, 20, 18, 20, 18, 1],
-        type: 'square'
-    };
-    
-    for (var i = 0; i < marinas.length; i++) {
-        var marinas = marinas[i];
-        var marker = new google.maps.Marker({
-            position: {lat: marinas[1], lng: marinas[2]},
-            map: map,
-            icon: image,
-            shape: shape,
-            title: marinas[0],
-            zIndex: marinas[3],
-            
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker ({
+            position: locations,
+            labels: labels[i % labels.length]
         });
-    }
+    });
     
-}
+    var markerClusterer = new MarkerClusterer (map, markers, {
+        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer/m'
+    });
 
+
+}
