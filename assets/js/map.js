1e484a87ccc15ddba87b4e02d64254aff5607957
EC2 Default User <ec2-user@ip-172-31-94-227.ec2.internal>
2019-12-10 01:11:15 +0000

    tried another way to render map markers

--- assets/js/map.js
+++ assets/js/map.js
@@ -4,24 +4,39 @@ function initMap() {
         center: { lat: 44.434546, lng: -79.350972 }
   
  });
-var label = "AB";
 
-var locations = [
-    { lat: 44.302020, lng: -79.472088 },
-    { lat: 44.308918, lng: -79.437413 }
+setMarkers(map);
+}
+var marinas = [
+    ['Island Grove Marina', 44.302020, -79.472088, 2],
+    ['Willow Beach Marina', 44.308918, -79.437413, 1],
     ];
     
-    var markers = locations.map(function(location, i) {
-        return new google.maps.Marker ({
-            position: locations,
-            label: label[i % label.length]
-        });
-    });
+    function setMarkers(map) {
+        var image = {
+            url: 'assets/images/anchor.png',
+            size: new google.maps.Size(30, 40),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(40, 0)
+        };
     
-    var markerClusterer = new MarkerClusterer (map, markers, {
-        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer/m'
-    });
-
-
+    var shape = {
+        coords: [1, 1, 1, 20, 18, 20, 18, 1],
+        type: 'square'
+    };
+    
+    for (var i = 0; i < marinas.length; i++) {
+        var marinas = marinas[i];
+        var marker = new google.maps.Marker({
+            position: {lat: marinas[1], lng: marinas[2]},
+            map: map,
+            icon: image,
+            shape: shape,
+            title: marinas[0],
+            zIndex: marinas[3],
+            
+        });
+    }
+    
 }
 
