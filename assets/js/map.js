ceada8a8cc94a1b78f5607672275a9dbee3f2656
EC2 Default User <ec2-user@ip-172-31-94-227.ec2.internal>
2019-12-10 00:32:33 +0000

    working on getting map markers to render

--- assets/js/map.js
+++ assets/js/map.js
@@ -2,10 +2,9 @@ function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 9,
         center: { lat: 44.434546, lng: -79.350972 }
-    });
-}
-
-var label = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  
+ });
+var label = "AB";
 
 var locations = [
     { lat: 44.302020, lng: -79.472088 },
@@ -22,4 +21,7 @@ var locations = [
     var markerClusterer = new MarkerClusterer (map, markers, {
         imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer/m'
     });
-    
+
+
+}
+
