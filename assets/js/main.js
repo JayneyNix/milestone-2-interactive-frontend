aa929cbc0973871b2158c932b8623de620e6523b
EC2 Default User <ec2-user@ip-172-31-94-227.ec2.internal>
2019-12-12 14:53:42 +0000

    make modal work from button, content & styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
-function infoPopup() {
-    document.getElementById("modal-info").modal('show');
+var modal = document.getElementById("serviceModal");
+var btn = document.getElementById("services");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
 }
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
